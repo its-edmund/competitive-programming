bab1d8468027b1745d1be12aa66deb52c4ed5c83
Edmund Xin <mxinburritos@gmail.com>
2025-01-29 20:46:16 -0800

    Add edu segment tree practice

---
+++ codeforces/edu/segment_tree/a_segment_tree_for_the_sum.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+vector<ll> tree;
+
+void solve() {
+    
+}
+
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n, m;
+    cin >> n >> m;
+    tree.resize(2 * n, 0)
+    while (n--) {
+        
+    }
+    for (int t = 1; t <= tc; t++) {
+        cout << "Case #" << t << ": ";
+        solve();
+    }
+}
